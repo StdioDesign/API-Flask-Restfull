4e7b3942c114e183f9aeecd3fea8ec2c23b8aec6
Darlan Nascimento <darlan.nascimento16@gmail.com>
2020-02-22 18:46:41 -0300

    habilidades.py alterada possuindo agora validação das habilidades

--- habilidades.py
+++ habilidades.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restful import  Resource
 import json
 
-lista_habilidades = [ {
+lista_habilidades = [{
         'id': 0,
         'habilidades': 'Python'
      },
@@ -33,6 +33,15 @@ class ListaHabilidades(Resource):
         return lista_habilidades[posicao]
 
 class Habilidades(Resource):
+    def get(self, id):
+        try:
+            response = lista_habilidades[id]
+        except IndexError:
+            response = {'status': 'erro', 'mensagem': f'Habilidade de ID {id} não existe'}
+        except Exception:
+            response = {'status': 'erro', 'mensagem': 'Erro desconhecido. Procure o administrador da API'}
+        return response
+
     def put(self, id):
         dados = json.loads(request.data)
         lista_habilidades[id] = dados
