ccff697a17490c58ef4cf23108acdccf63db4076
Darlan Nascimento <darlan.nascimento16@gmail.com>
2020-02-22 18:40:50 -0300

    Incluido requirements do projeto, o app_restful com um exemplo de aplicação com a extensão flask-restful e incluido o módulo habilidade para gerenciar as habilidades que podem ser associadas ao desenvolvedor

---
+++ habilidades.py
@@ -0,0 +1,43 @@
+from flask import request
+from flask_restful import  Resource
+import json
+
+lista_habilidades = [ {
+        'id': 0,
+        'habilidades': 'Python'
+     },
+    {
+        'id': 1,
+        'habilidades': 'Flask'
+    },
+    {
+        'id': 2,
+        'habilidades': 'Java'
+    },
+    {
+        'id': 3,
+        'habilidades': 'PHP'
+    }
+
+]
+
+class ListaHabilidades(Resource):
+    def get(self):
+        return lista_habilidades
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(lista_habilidades)
+        dados['id'] = posicao
+        lista_habilidades.append(dados)
+        return lista_habilidades[posicao]
+
+class Habilidades(Resource):
+    def put(self, id):
+        dados = json.loads(request.data)
+        lista_habilidades[id] = dados
+        return dados
+
+    def delete(self, id):
+        lista_habilidades.pop(id)
+        return {'status': 'sucesso', 'mensagem': 'Registro excluído'}
